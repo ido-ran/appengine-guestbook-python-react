9ee3225941640adf20971a6ea4a7afcf5a1ed961
Ido Ran <ido.ran@gmail.com>
2016-11-24 23:15:13 +0200

    wip - implement guestbook

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {payload:'no set yet'}
+  }
+
+  componentWillMount() {
+    const that = this;
+    fetch('/api/frames').then(function(response) {
+      var contentType = response.headers.get("content-type");
+      if(contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json().then(function(json) {
+          that.setState(json)
+        });
+      } else {
+        console.log("Oops, we haven't got JSON!");
+      }
+    });
+  }
+
   render() {
+    const {payload} = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React - {payload}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
