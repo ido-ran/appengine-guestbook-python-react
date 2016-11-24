2826567210d03acbe4700d37fd687b2df0b6762e
Ido Ran <ido.ran@gmail.com>
2016-11-24 23:15:13 +0200

    Implement guestbook sample api

--- main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-from streams_handler import StreamsHandler
-from frames_handler import FramesHandler
-
-app = webapp2.WSGIApplication([
-    ('/api/streams', StreamsHandler),
-    ('/api/frames', FramesHandler),
-], debug=True)
