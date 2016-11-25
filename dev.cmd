b8460ffc3ae7331c96cfabdbd1d756abf8cfe3e1
Ido Ran <ido.ran@gmail.com>
2016-11-25 22:25:48 +0200

    Move server app to subdirectory to overcome gcloud deploy all files including .git directory

--- dev.cmd
+++ dev.cmd
@@ -1,1 +1,1 @@
-python "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" .
+python "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" server
